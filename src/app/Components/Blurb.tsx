9c51e0e2cdc859f9a580af3e00670f687572ff24
Matt Brevetti <matt.soundli@gmail.com>
2024-01-24 00:30:45 -0500

    created Blurb icon and mapped on homepage

--- src/app/Components/Blurb.tsx
+++ src/app/Components/Blurb.tsx
@@ -6,11 +6,13 @@ type BlurbProps = {
     icon: ReactElement;
     headline: string;
     url: string;
+    className?: string;
 };
 
-const Blurb: React.FC<BlurbProps> = ({ icon, headline, url }) => {
+const Blurb: React.FC<BlurbProps> = ({ icon, headline, url, className }) => {
+    const baseStyles = "flex flex-col items-center p-4 py-12 border border-blue-300 rounded-lg hover:shadow-lg transition-shadow duration-300";
     return (
-        <Link href={url} className="flex flex-col items-center p-4 border border-gray-200 rounded-lg hover:shadow-lg transition-shadow duration-300">
+        <Link href={url} className={`${baseStyles} ${className}`}>
             <div className="text-4xl mb-2">{icon}</div>
             <div className="text-xl font-semibold">{headline}</div>
         </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,108 +1,133 @@
-import Image from 'next/image'
-import Navbar from './Components/Navbar'
-import HomeHero from './Components/HomeHero'
-import Blurb from './Components/Blurb'
-import { BeakerIcon, BriefcaseIcon, DocumentTextIcon } from '@heroicons/react/24/outline'
+import Image from "next/image";
+import Navbar from "./Components/Navbar";
+import HomeHero from "./Components/HomeHero";
+import Blurb from "./Components/Blurb";
+import {
+  BeakerIcon,
+  BriefcaseIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/24/outline";
+
+const blurbNav = [
+  {
+    name: "My Experience",
+    href: "/experience",
+    icon: <BeakerIcon className="w-12" />,
+  },
+  {
+    name: "Portfolio",
+    href: "/portfolio",
+    icon: <BriefcaseIcon className="w-12" />,
+  },
+  {
+    name: "Resume",
+    href: "/resume",
+    icon: <DocumentTextIcon className="w-12" />,
+  },
+  { name: "About", href: "/contact", icon: <BeakerIcon className="w-12" /> },
+  {
+    name: "Web Design Work",
+    href: "/web",
+    icon: <BriefcaseIcon className="w-12" />,
+  },
+  {
+    name: "Recent Projects",
+    href: "/recent",
+    icon: <DocumentTextIcon className="w-12" />,
+  },
+];
 
 export default function Home() {
-  
   return (
     <>
-    <HomeHero />
-    <Navbar />
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <HomeHero />
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <div className="w-full bg-blue-400 py-24 text-blue-900">
+          <div className="container mx-auto p-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {blurbNav.map((item) => (
+                <Blurb
+                  key={item.name}
+                  icon={item.icon}
+                  headline={item.name}
+                  url={item.href}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
 
-    <div className="container mx-auto p-4">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <Blurb
-          icon={<BeakerIcon />}
-          headline="My Experience"
-          url="/experience"
-        />
-        <Blurb
-          icon={<BriefcaseIcon />}
-          headline="My Resume"
-          url="/resume"
-        />
-        <Blurb
-          icon={<DocumentTextIcon />}
-          headline="Portfolio"
-          url="/portfolio"
-        />
-        {/* Add more `Blurb` components as needed */}
-      </div>
-    </div>
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Docs{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Find in-depth information about Next.js features and API.
+            </p>
+          </a>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Learn{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Learn about Next.js in an interactive course with&nbsp;quizzes!
+            </p>
+          </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Templates{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Explore starter templates for Next.js.
+            </p>
+          </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Deploy{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Instantly deploy your Next.js site to a shareable URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
     </>
-  )
+  );
 }
