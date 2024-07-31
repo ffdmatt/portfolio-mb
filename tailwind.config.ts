d94fd18b368ce24ea352774167803925c66ef8ed
Matt Brevetti <matt.soundli@gmail.com>
2024-07-31 16:09:14 -0400

    tailwind config fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,14 +17,7 @@ const config: Config = {
     },
   },
   plugins: [
-    plugin(function({ addBase, theme }) {
-      addBase({
-        'h1': { fontSize: theme('fontSize.4xl') },
-        'h2': { fontSize: theme('fontSize.3xl') },
-        'h3': { fontSize: theme('fontSize.2xl') },
-        'h4': { fontSize: theme('fontSize.lg') },
-      })
-    })
+   
   ],
   darkMode: 'class',
 }
